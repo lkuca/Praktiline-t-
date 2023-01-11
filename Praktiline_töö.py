72565e668806de776111879a1b91c8c257f63916
opilane <opilane@HP-CZC2349HTQ>
2023-01-11 12:31:18 +0200

    Add project files.

---
+++ Praktiline_töö.py
@@ -0,0 +1,17 @@
+#for x in range (1,101,1):
+#   if x%5==0:
+#       print(x, end=" ")
+
+#ülesanne 0
+while 1:
+    try:
+        a=(abs(int(input("to start cycle press 1 "))))
+    except ValueError :
+        print()
+    if a==1 :
+        print("edasi")
+    elif a==str:
+            break
+    else: break
+
+     
